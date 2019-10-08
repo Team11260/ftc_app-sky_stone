e0dc72fed798b9de9d9d9a4dbed289130929e683
AJFrio <af4family@gmail.com>
2019-10-07 21:09:20 -0600

    Updated vision software

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum/opmodes/test/BlockFindAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanum/opmodes/test/BlockFindAuton.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.mecanum.opmodes.test;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.util.SparseIntArray;
 import android.widget.ImageView;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -13,6 +14,8 @@ import org.firstinspires.ftc.teamcode.framework.userhardware.inputs.sensors.visi
 import org.firstinspires.ftc.teamcode.framework.userhardware.inputs.sensors.vision.vuforia.VuforiaImpl;
 import org.upacreekrobotics.dashboard.Config;
 
+import java.util.HashMap;
+
 @Autonomous(name = "BlockFind Auton", group = "New")
 
 @Config
@@ -20,17 +23,43 @@ import org.upacreekrobotics.dashboard.Config;
 
 public class BlockFindAuton extends AbstractAuton {
 
+
     private ImageProcessor imageProcessor;
-    int XORIGIN = 250;
-    int YORIGIN = 240;
-    int BLOCKWIDTH = 220;
-    int BLOCKHEIGHT = 110;
+    int XORIGIN = 205;
+    int YORIGIN = 330;
+    int BLOCKWIDTH = 295;
+    int BLOCKHEIGHT = 148;
     int LINEWIDTH = 10;
+
+
+    int threshold = 100;
+    // int height = YORIGIN + (BLOCKHEIGHT/2);
+    int height = YORIGIN+10;
+    int x_left = XORIGIN + 50;
+    int x_center = x_left + BLOCKWIDTH;
+    int x_right = x_center + BLOCKWIDTH;
 
     @Override
     public void RegisterStates() {
 
     }
+
+    /* public void InitLoop() {
+         ImageView userImage=null;
+
+         Bitmap image = null;
+
+         image = imageProcessor.getImage();
+
+         telemetry.addData(DoubleTelemetry.LogMode.INFO, getSkyStonePositionThreeStones(image));
+
+         telemetry.update();
+
+
+         ImageProcessor.drawBox(image, XORIGIN, YORIGIN, 3 * BLOCKWIDTH, BLOCKHEIGHT, LINEWIDTH, Color.rgb(0, 0, 225));
+
+         imageProcessor.setImage(image);
+*/
 
     @Override
     public void Init() {
@@ -44,8 +73,8 @@ public class BlockFindAuton extends AbstractAuton {
     @Override
     public void Run() {
 
+        ImageView userImage = null;
         Bitmap image = null;
-        //ImageView userImage=null;
 
         telemetry.addData(DoubleTelemetry.LogMode.INFO, "run started" + opModeIsActive());
         telemetry.update();
@@ -62,16 +91,16 @@ public class BlockFindAuton extends AbstractAuton {
             telemetry.addData(DoubleTelemetry.LogMode.INFO, Color.blue(image.getPixel(XORIGIN + 260, 355)));
 
             telemetry.addData(DoubleTelemetry.LogMode.INFO, Color.red(image.getPixel(XORIGIN + 480, 355)));*/
-           // telemetry.addData(DoubleTelemetry.LogMode.INFO, getFour(image,305, 295));
+            // telemetry.addData(DoubleTelemetry.LogMode.INFO, getFour(image,305, 295));
             //telemetry.addData(DoubleTelemetry.LogMode.INFO, getFour(image,525, 295));
             //telemetry.addData(DoubleTelemetry.LogMode.INFO, getFour(image,745, 295));
 
 
-           // telemetry.addData(DoubleTelemetry.LogMode.INFO, Color.red(image.getPixel(525, 295)));
-           // telemetry.addData(DoubleTelemetry.LogMode.INFO, Color.red(image.getPixel(745, 295)));
+            // telemetry.addData(DoubleTelemetry.LogMode.INFO, Color.red(image.getPixel(525, 295)));
+            // telemetry.addData(DoubleTelemetry.LogMode.INFO, Color.red(image.getPixel(745, 295)));
 
 
-           // telemetry.addData(DoubleTelemetry.LogMode.INFO, "New Round of Data");
+            // telemetry.addData(DoubleTelemetry.LogMode.INFO, "New Round of Data");
             for (int i = 0; i < 3; i++) {
                 // telemetry.addData(DoubleTelemetry.LogMode.INFO, "" +Color.red(image.getPixel(305+i*220, 295 )));
             }
@@ -81,8 +110,10 @@ public class BlockFindAuton extends AbstractAuton {
             telemetry.update();
 
             ImageProcessor.drawBox(image, XORIGIN, YORIGIN, 3 * BLOCKWIDTH, BLOCKHEIGHT, LINEWIDTH, Color.rgb(0, 0, 225));
-           /* ImageProcessor.drawBox(image, 295, 260, 20, 20, LINEWIDTH, Color.rgb(225, 0, 0));
-            ImageProcessor.drawBox(image, 295, 310, 20, 20, LINEWIDTH, Color.rgb(225, 0, 0));
+            ImageProcessor.drawBox(image, XORIGIN + 30, YORIGIN + 10, 1, BLOCKHEIGHT - 50, LINEWIDTH = 4, Color.rgb(225, 0, 0));
+            ImageProcessor.drawBox(image, XORIGIN + 30+BLOCKWIDTH, YORIGIN + 10, 1, BLOCKHEIGHT - 50, LINEWIDTH = 4, Color.rgb(225, 0, 0));
+            ImageProcessor.drawBox(image, XORIGIN + 30+BLOCKWIDTH+BLOCKWIDTH, YORIGIN + 10, 1, BLOCKHEIGHT - 50, LINEWIDTH = 4, Color.rgb(225, 0, 0));
+            /*ImageProcessor.drawBox(image, 295, 310, 20, 20, LINEWIDTH, Color.rgb(225, 0, 0));
             ImageProcessor.drawBox(image, 425, 260, 20, 20, LINEWIDTH, Color.rgb(225, 0, 0));
             ImageProcessor.drawBox(image, 425, 310, 20, 20, LINEWIDTH, Color.rgb(225, 0, 0));
             ImageProcessor.drawBox(image, 515, 260, 20, 20, LINEWIDTH, Color.rgb(225, 0, 0));
@@ -102,19 +133,48 @@ public class BlockFindAuton extends AbstractAuton {
 
         int threshold = 100;
         // int height = YORIGIN + (BLOCKHEIGHT/2);
-        int height = 295;
-        int x_left = 305;
+        int height = YORIGIN+10;
+        int x_left = XORIGIN + 30;
         int x_center = x_left + BLOCKWIDTH;
         int x_right = x_center + BLOCKWIDTH;
 
-        if (getFour(image, x_left, height) < threshold)
+        if (getLineAverage(image, x_left, height) < threshold)
             return "Left";
-        else if (getFour(image, x_center, height) < threshold)
+        else if (getLineAverage(image, x_center, height) < threshold)
             return "Center";
-        else if (getFour(image, x_right, height) < threshold)
+        else if (getLineAverage(image, x_right, height) < threshold)
             return "Right";
         else
             return "No Sky Stone Found";
+    }
+
+    public int getLineAverage(Bitmap image, int x, int y) {
+        int sum = 0;
+        for (int i = 0; i < BLOCKHEIGHT - 50; i++)
+            sum += Color.red(image.getPixel(x, y + i));
+        sum /= (19 + BLOCKHEIGHT);
+        telemetry.addData(DoubleTelemetry.LogMode.INFO,sum);
+        return sum;
+    }
+
+    public int getLineMode(Bitmap image, int x, int y) {
+        int mode = 0;
+        Integer current = 0;
+        SparseIntArray hashmap = new SparseIntArray();
+        for (int i = 0; i < BLOCKHEIGHT - 20; i++) {
+            current = hashmap.get(Color.red(image.getPixel(x, y + i)));
+            hashmap.put(Color.red(image.getPixel(x, y + i)), ++current);
+        }
+        int greatest = 0;
+        int color = 0;
+        for (int i = 0; i < hashmap.size(); i++) {
+            if (hashmap.valueAt(i) > greatest) {
+                greatest = hashmap.valueAt(i);
+                color = hashmap.keyAt(i);
+            }
+
+        }
+        return color;
     }
 
 
@@ -137,10 +197,10 @@ public class BlockFindAuton extends AbstractAuton {
         int yoffset = 20;
         int xoffset = 130;
 
-        sum = getPixelNineAve(image, x , y-yoffset);
-        sum = sum + getPixelNineAve(image, x , y+yoffset);
-        sum = sum + getPixelNineAve(image, x+xoffset , y-yoffset);
-        sum = sum + getPixelNineAve(image, x+xoffset , y+yoffset);
+        sum = getPixelNineAve(image, x, y - yoffset);
+        sum = sum + getPixelNineAve(image, x, y + yoffset);
+        sum = sum + getPixelNineAve(image, x + xoffset, y - yoffset);
+        sum = sum + getPixelNineAve(image, x + xoffset, y + yoffset);
 
         return sum / 4;
         //return Color.red(image.getPixel(x , y ));
